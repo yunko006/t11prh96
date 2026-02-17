fec808442e44b36466c40fe6944db9680463e60d
yunko006 <peyrachethomas@gmail.com>
2026-02-17 18:36:39 +0100

    Update test_about.py

--- backend/tests/api/routes/test_about.py
+++ backend/tests/api/routes/test_about.py
@@ -1,7 +1,15 @@
 from fastapi.testclient import TestClient
+from app.schemas.about import About
+
+me_test = About(
+    nom="prh",
+    prenom="tom",
+    role="backend dev",
+    description="bonjour ceci est un test de desc",
+)
 
 
 def test_get_about(client: TestClient) -> None:
     response = client.get("/about")
     assert response.status_code == 200
-    assert response.json() == {"pseudo": "t11prh96"}
+    assert response.json() == me_test.model_dump()

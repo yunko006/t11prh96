cf63615ca8673a214271072a8620ffcd9ec52966
yunko006 <peyrachethomas@gmail.com>
2026-02-17 18:36:34 +0100

    Update about to get data from json file

--- backend/app/api/routes/about.py
+++ backend/app/api/routes/about.py
@@ -1,9 +1,17 @@
+import json
+from pathlib import Path
+
 from fastapi import APIRouter
 
+from app.schemas.about import About
 
 router = APIRouter()
 
+DB_PATH = Path(__file__).parents[4] / "data" / "db.json"
 
-@router.get("/about")
+
+@router.get("/about", response_model=About)
 async def about():
-    return {"pseudo": "t11prh96"}
+    with open(DB_PATH) as f:
+        data = json.load(f)
+    return data["about"]
